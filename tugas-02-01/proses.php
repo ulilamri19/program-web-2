7eaf07c0c9b1492b520d9c1957fd2ab33e7a2267
ulil amri <ulilamri19@mhs.akba.ac.id>
2021-11-03 15:09:57 +0700

    commit pertama

---
+++ tugas-02-01/proses.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Hai, perkenalkan nama saya ".$_POST['Nama']." 
+    dengan NIM ".$_POST['NIM'].". Saya adalah seorang 
+    mahasiswa dari program studi ".$_POST['Prodi'].".";
+?>
---
+++ tugas-02-01/variabel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Ini adalah nilai awal variabel
+*/
+$nama = "ulil amri";
+$usia = 21;
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya
+berusia ".$usia." tahun<p>";
+/**
+* Ini adalah variabel dengan nilai yang diubah
+*/
+$nama = ISI_NAMA_ANDA
+$usia = ISI_USIA_ANDA
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya
+berusia ".$usia." tahun<p>";
+?>
